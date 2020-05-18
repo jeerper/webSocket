64c924a8f7805dc82d56164fe8a91f960c499d80
qianyy <2997272453@qqcom>
2020-05-18 10:01:44 +0800

    提交代码

---
+++ src/test/java/com/summit/SummitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
